653e852e1e6151a01df53e52b70e23c7d7824265
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-06-07 19:52:34 +0530

    Time: 4 ms (37.25%), Space: 6.1 MB (13.55%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int n=start^goal;
-        long long count=0;
-        while(n>0){
-            count++;
-            n&=(n-1);
-        }
-        return count;
+        return __builtin_popcount(start^goal);
     }
 };
