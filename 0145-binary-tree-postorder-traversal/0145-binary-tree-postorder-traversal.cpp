84d93e7d04c4d77cd3d03152fa6bd35a212df52b
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-07-06 20:17:42 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (50.91%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,15 +11,23 @@
  */
 class Solution {
 public:
-    void post(TreeNode * root, vector<int> & res){
-        if(root==NULL)  return;
-        post(root->left,res);
-        post(root->right,res);
-        res.push_back(root->val);
-    }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
-        post(root,res);
+        stack<TreeNode* > st1,st2;
+        if(root==NULL)  return res;
+        
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* tmp=st1.top();
+            st1.pop();
+            st2.push(tmp);
+            if(tmp->left)   st1.push(tmp->left);
+            if(tmp->right)   st1.push(tmp->right);
+        }
+        while(!st2.empty()){
+            res.push_back(st2.top()->val);
+            st2.pop();
+        }
         return res;
     }
 };
