08a4448b7b9dfa0fa28ae81237e8e41d3eebf967
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-07-11 22:02:16 +0530

    Time: 47 ms (16.57%), Space: 32.6 MB (49.52%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -9,33 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+class Solution 
+{
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if(!root)   return NULL;
-        if(key<root->val){
-            root->left=deleteNode(root->left,key);
+    TreeNode* deleteNode(TreeNode* root, int key) 
+    {
+       if(root==NULL) return NULL;
+       if(root->val==key)
+        { return helper(root);
+        }        
+        TreeNode* dummy=root;
+        while(root!=NULL)
+        {
+            if(root->val>key)
+            {
+                if(root->left!=NULL&& root->left->val==key)
+                {
+                    root->left=helper(root->left);
+                    break;
+                }
+                else
+                {root=root->left;
+                }
         }
-        else if(key>root->val){
-            root->right=deleteNode(root->right,key);
+        else
+        {
+            if(root->right!=NULL&& root->right->val==key)
+            {
+            root->right=helper(root->right);
+            break;
+            }
+            else
+            {
+                root=root->right;
+            } 
         }
-        else{
-            if(!root->left && !root->right){
-                delete(root);
-                return NULL;
-            }
-            else if(!root->left || !root->right){
-                TreeNode* tmp= root->left? root->left: root->right;
-                delete(root);
-                return tmp;
-            }
-            else{
-                TreeNode* tmp=root->right;
-                while(tmp->left)    tmp=tmp->left;
-                root->val=tmp->val;
-                root->right=deleteNode(root->right,tmp->val);
-            }
-        }
-        return root;
     }
-};
+    return dummy;
+}
+TreeNode* helper(TreeNode* root){
+    if(root->left==NULL){return root->right;}
+    else if(root->right==NULL){return root->left;}
+     TreeNode* rightchild=root->right;
+     TreeNode* lastright=lastrightchild(root->left);
+     lastright->right=rightchild;
+     return root->left;
+}
+TreeNode* lastrightchild(TreeNode* root){
+    if(root->right==NULL){return root;}
+  return lastrightchild(root->right);}
+  };
