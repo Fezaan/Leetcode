15fda3449876c91dcd84bbf997c0d2d0b524428a
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-07-15 19:16:14 +0530

    Time: 31 ms (41.25%), Space: 15.3 MB (27.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+    void dfs(int node,vector<vector<int>>& con, vector<int>& vis){
+        vis[node]=1;
+        for(int i=0;i<con.size();i++){
+            if(con[node][i]==1 && !vis[i]){
+                dfs(i,con,vis);
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& con) {
         int n=con.size();
         vector<int> vis(n,0);
         int cnt=0;
-        queue<int> q;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                q.push(i);
-                while(!q.empty()){
-                    int tmp=q.front();
-                    q.pop();
-                    vis[tmp]=1;
-                    for(int j=0;j<n;j++){
-                        if(con[tmp][j]==1 && !vis[j])   q.push(j);
-                    }
-                }
+            if(vis[i]==0){
+                dfs(i,con,vis);
                 cnt++;
             }
         }
