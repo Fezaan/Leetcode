dab03249d2c0bb18773415cb5c5ef567e53512f4
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-07-19 15:51:11 +0530

    Time: 240 ms (22.91%), Space: 58.8 MB (52.79%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&adj,int src,vector<bool>&vis,vector<bool>&recst){
-        vis[src]=true;
-        recst[src]=true;
-        for(auto x:adj[src]){
-            if(vis[x]==false && dfs(adj,x,vis,recst)){
-                return true;
-            }
-            else if(recst[x]==true){
-            return true;
-            }
-        }
-        recst[src]=false;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-         vector<vector<int>>adj(n);
-        for(int i=0;i<graph.size();i++){
-            for(int j=0;j<graph[i].size();j++){
-                adj[i].push_back(graph[i][j]);
-            }
-        }
-            vector<bool>vis(n,false);
-        vector<bool>recst(n,false);
+        vector<int> adj[n+1];
         for(int i=0;i<n;i++){
-            if(vis[i]==false){
-                dfs(adj,i,vis,recst);
+            for(auto it: graph[i])  adj[it].push_back(i);
+        }
+        queue<int> q;
+        vector<int> res;
+        vector<int> indeg(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it: adj[i])    indeg[it]++;
+        }
+        for(int i=0;i<n;i++)    if(indeg[i]==0) q.push(i);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            res.push_back(node);
+            for(auto it: adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)    q.push(it);
             }
         }
-        vector<int>ans;
-        for(int i=0;i<recst.size();i++){
-          if(!recst[i])
-          ans.push_back(i);
-        }
-        return ans;
+        sort(res.begin(),res.end());
+        return res;
     }
 };
