275cdd3edd299368309564c15e2f92b1ff0fc1bc
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-07-31 15:14:25 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (48.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,7 +9,20 @@ public:
         return dp[m][n]=l+u;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return f(m-1,n-1,dp);
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        
+        for(int i=0 ; i<m ; i++){
+            for(int j=0 ; j<n ; j++){
+                if(i==0 && j==0)    dp[0][0]=1;
+                else{
+                    int l=0,u=0;
+                    if(i>0) u=dp[i-1][j];
+                    if(j>0) l=dp[i][j-1];
+                    dp[i][j]=l+u;
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
