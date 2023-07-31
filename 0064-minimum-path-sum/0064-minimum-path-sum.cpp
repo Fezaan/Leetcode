6c0b9ca689096e0bdc90af09fe1b4ada299e767b
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-07-31 22:06:42 +0530

    Time: 7 ms (92.56%), Space: 9.9 MB (71.43%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -8,10 +8,28 @@ public:
         int u=f(i-1,j,grid,dp);
         return dp[i][j]=grid[i][j]+min(l,u);
     }
-    int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return f(m-1,n-1,grid,dp);
+    int minPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int> > dp(n,vector<int>(m,0));
+        for(int i=0; i<n ; i++){
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0) dp[i][j] = matrix[i][j];
+                else{
+
+                    int up = matrix[i][j];
+                    if(i>0) up += dp[i-1][j];
+                    else up += 1e9;
+
+                    int left = matrix[i][j];
+                    if(j>0) left+=dp[i][j-1];
+                    else left += 1e9;
+
+                    dp[i][j] = min(up,left);
+                }
+            }
+        }
+    
+        return dp[n-1][m-1];
     }
 };
