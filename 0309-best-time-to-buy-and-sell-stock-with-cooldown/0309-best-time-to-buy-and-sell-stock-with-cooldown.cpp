b389d896a39fcca088dcb6749b78c07396a01db2
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-08-16 18:29:31 +0530

    Time: 0 ms (100.00%), Space: 12 MB (23.63%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int f(int i, int j, int n, vector<int>& arr, vector<vector<int>>& dp){
-        if(i>=n)    return 0;
+        if(i==n || i==n+1 || i==n+2)    return 0;
         if(dp[i][j]!=-1)    return dp[i][j];
         if(j)   return dp[i][j]=max(f(i+1,0,n,arr,dp)-arr[i], f(i+1,1,n,arr,dp));
         return dp[i][j]=max(f(i+2,1,n,arr,dp)+arr[i], f(i+1,0,n,arr,dp));
