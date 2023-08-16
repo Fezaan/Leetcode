b0578c6f93c6dbeab440b2ef9c15c224b91ceb63
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-08-16 18:35:55 +0530

    Time: 7 ms (42.41%), Space: 12.1 MB (13.40%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -8,7 +8,20 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1, vector<int> (2,-1));
-        return f(0,1,n,prices,dp);
+        vector<vector<int>> dp(n+2, vector<int> (2,0));
+        // for(int i=n, i<=n+3;i++){
+        //     for(iint j=0;i<2;i++)   dp[i][j]=0;
+        // }
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j)   dp[i][j]=max(dp[i+1][0]-prices[i], dp[i+1][1]);
+                else    dp[i][j]=max(dp[i+2][1]+prices[i], dp[i+1][0]);
+            }
+        }
+        for(auto it: dp){
+            for(auto j: it) cout<<j<<" ";
+            cout<<endl;
+        }
+        return dp[0][1];
     }
 };
