bc5e5524936434e120afdb08d32845c91f2642c3
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-08-24 09:59:22 +0530

    Time: 41 ms (93.40%), Space: 10.5 MB (13.55%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -15,7 +15,18 @@ public:
         cuts.insert(cuts.begin(),0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(m+3, vector<int> (m+3,-1));
-        return f(1,m,cuts,dp);
+        vector<vector<int>> dp(m+3, vector<int> (m+3,0));
+        for(int i=m;i>0;i--){
+            for(int j=1;j<=m;j++){
+                if(i>j) continue;
+                long mn=1e9;
+                for(int k=i;k<=j;k++){
+                    long cost=cuts[j+1]-cuts[i-1]+ dp[i][k-1]+ dp[k+1][j];
+                    mn=min(mn,cost);
+                }
+                dp[i][j]=mn;
+            }
+        }
+        return dp[1][m];
     }
 };
