7fbd3b32660cf94b8d0ac441ecb16b584c2773b1
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-08-25 23:31:45 +0530

    Time: 136 ms (83.00%), Space: 81.4 MB (59.57%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-      stack < int > st;
-      int leftsmall[n], rightsmall[n];
-      for (int i = 0; i < n; i++) {
-        while (!st.empty() && heights[st.top()] >= heights[i]) {
-          st.pop();
+        int n=heights.size();
+        vector<int> prefix(n),suffix(n);
+        
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            if(!st.empty()) prefix[i]=i-st.top();
+            else    prefix[i]=i+1;
+            st.push(i);
         }
-        if (st.empty())
-          leftsmall[i] = 0;
-        else
-          leftsmall[i] = st.top() + 1;
-        st.push(i);
-      }
-      // clear the stack to be re-used
-      while (!st.empty())
-        st.pop();
-
-      for (int i = n - 1; i >= 0; i--) {
-        while (!st.empty() && heights[st.top()] >= heights[i])
-          st.pop();
-
-        if (st.empty())
-          rightsmall[i] = n - 1;
-        else
-          rightsmall[i] = st.top() - 1;
-
-        st.push(i);
-      }
-      int maxA = 0;
-      for (int i = 0; i < n; i++) {
-        maxA = max(maxA, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
-      }
-      return maxA;
+        while(!st.empty())  st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+            if(!st.empty()) suffix[i]=st.top()-i;
+            else suffix[i]=n-i;
+            st.push(i);
+        }
+        int mx=INT_MIN;
+        for(int i=0;i<n;i++){
+            int cost= (suffix[i]+prefix[i]-1)*heights[i];
+            mx=max(mx,cost);
+        }
+        return mx;
     }
 };
