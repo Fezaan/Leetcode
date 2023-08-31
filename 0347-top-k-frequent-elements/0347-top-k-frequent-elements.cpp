cd4863e6192f7a0054ad8c780daa24f8e582acb9
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-08-31 11:17:43 +0530

    Time: 16 ms (51.74%), Space: 13.8 MB (23.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,14 +3,13 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
-        priority_queue<p,vector<p>,greater<p>> pq;
-        // priority_queue<p> pq;
+        vector<int> res;
+        priority_queue<p, vector<p> , greater<p>> pq;
         for(auto it: nums)  mp[it]++;
         for(auto it: mp){
             pq.push({it.second,it.first});
             if(pq.size()>k) pq.pop();
         }
-        vector<int> res;
         while(!pq.empty()){
             res.push_back(pq.top().second);
             pq.pop();
