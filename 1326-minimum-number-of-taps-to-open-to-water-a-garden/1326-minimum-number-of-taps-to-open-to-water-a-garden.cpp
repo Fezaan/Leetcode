847903490b58b9bf7518c93db6c27f8ff4c053d8
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-09-01 19:45:01 +0530

    Time: 27 ms (66.71%), Space: 14.7 MB (55.48%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+    // int f(int i, vector<int>& ranges, vector<int>& dp){
+    //     if(i==0)    return 0;
+    //     if(dp[i]!=-1)   return dp[i];
+    //     int p=1e9;
+    //     int np=f(i-1,ranges,dp);
+    //     if()    p=1+f(i-1,ranges,dp);
+    //     return dp[i]=min(p,np);
+    // }
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> arr(n + 1, 0);
-        for(int i = 0; i < ranges.size(); ++i) {
-            if(ranges[i] == 0) continue;
-            int left = max(0, i - ranges[i]);
-            arr[left] = max(arr[left], i + ranges[i]);
+        vector<int> res(n+1,0);
+        for(int i=0;i<=n;i++){
+            if(ranges[i]==0)    continue;
+            int pick=max(0,i-ranges[i]);
+            res[pick]=max(res[pick],i+ranges[i]);
         }
-        
-        int end = 0, far_can_reach = 0, cnt = 0;
-        for(int i = 0; i <= n; ++i) {
-            if(i > end) {
-                if(far_can_reach <= end) return -1;
-                end = far_can_reach;
-                ++cnt;
+        for(auto it: res)   cout<<it<<" ";
+        int end=0, range=0, cnt=0;
+        for(int i=0;i<n+1;i++){
+            if(i>end){
+                if(range<=end)    return -1;
+                end=range;
+                cnt++;  
             }
-            far_can_reach = max(far_can_reach, arr[i]);
+            range=max(range,res[i]);
         }
-        
-        return cnt + (end < n);
+        return cnt;
     }
 };
