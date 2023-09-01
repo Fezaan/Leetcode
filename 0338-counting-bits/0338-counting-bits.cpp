961d0f6b82a89bd9106ab97af14880d5efd765d4
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-09-01 20:13:17 +0530

    Time: 3 ms (89.82%), Space: 7.9 MB (51.17%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int cntBits(int n){
-        int cnt=0;
-        while(n){
-            cnt+=n&1;
-            n=n>>1;
-        }
-        return cnt;
-    }
+    // int cntBits(int n){
+    //     int cnt=0;
+    //     while(n){
+    //         cnt+=n&1;
+    //         n=n>>1;
+    //     }
+    //     return cnt;
+    // }
     vector<int> countBits(int n) {
         vector<int> res(n+1,0);
         for(int i=1;i<=n;i++){
-            res[i]=(cntBits(i));
+            res[i]=__builtin_popcount(i);
         }
         return res;
     }
