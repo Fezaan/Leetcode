e51531dd8eb966b0f0c1b2b15098e91a4be05575
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-09-01 19:58:29 +0530

    Time: 3 ms (89.82%), Space: 8 MB (45.56%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int cntBits(int n){
+        int cnt=0;
+        while(n){
+            cnt+=n&1;
+            n=n>>1;
+        }
+        return cnt;
+    }
+    vector<int> countBits(int n) {
+        vector<int> res(n+1,0);
+        for(int i=1;i<=n;i++){
+            res[i]=(cntBits(i));
+        }
+        return res;
+    }
+};
