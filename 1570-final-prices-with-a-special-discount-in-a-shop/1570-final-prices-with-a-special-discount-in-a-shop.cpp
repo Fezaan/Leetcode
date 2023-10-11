25caa75d3a30bbfdb3650952644d7b778eedc312
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-10-11 13:10:52 +0530

    Time: 3 ms (76.18%), Space: 10.3 MB (27.19%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int> res;
+        vector<int> res(prices);
+        stack<int> st;
         int n=prices.size();
         for(int i=0;i<n;i++){
-                bool f=0;
-            for(int j=i+1;j<n;j++){
-                if(prices[i]>=prices[j]){
-                    res.push_back(prices[i]-prices[j]);
-                    f=1;
-                    break;
+            if(st.empty() || prices[st.top()]<prices[i]) st.push(i);
+            else{
+                while(!st.empty() && prices[st.top()]>=prices[i]){
+                    res[st.top()]-=prices[i];
+                    st.pop();
                 }
+                st.push(i);
             }
-                if(f==0)    res.push_back(prices[i]);
         }
         return res;
     }
