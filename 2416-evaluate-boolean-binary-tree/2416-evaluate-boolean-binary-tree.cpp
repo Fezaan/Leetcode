438e398f67a3c85cd6e476aa8bd839b1c03769ce
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-10-15 15:35:44 +0530

    Time: 12 ms (47.49%), Space: 15.3 MB (19.06%) - LeetHub

---
+++ 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(!root->left && !root->right)  return root->val==1;
+        if(root->val==2){
+            bool l=evaluateTree(root->left);
+            bool r=evaluateTree(root->right);
+            return l|r;
+        }
+            bool l=evaluateTree(root->left);
+            bool r=evaluateTree(root->right);
+            return l&r;
+    }
+};
