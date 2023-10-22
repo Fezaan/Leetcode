517cddac58558943251a95a5499f9e1aac14d806
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-10-22 11:08:46 +0530

    Time: 4 ms (50.93%), Space: 9.5 MB (16.91%) - LeetHub

--- 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>,greater<int>> pq(nums.begin(),nums.end());
         while(k--){
-            *min_element(nums.begin(),nums.end())*=-1;
+            int tmp=pq.top();
+            pq.pop();
+            pq.push(tmp*-1);
         }
         int sum=0;
-        for(auto it: nums)  sum+=it;
+        while(!pq.empty()){
+            sum+=pq.top();
+            pq.pop();
+        }
         return sum;
     }
 };
