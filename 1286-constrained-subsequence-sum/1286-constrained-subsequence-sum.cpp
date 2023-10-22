c4d58666bdee6abac31d0ad5b329b35a5b3e2cdc
StuckCoder <110244796+Fezaan@users.noreply.github.com>
2023-10-22 15:38:21 +0530

    Time: 297 ms (31.96%), Space: 139.6 MB (42.06%) - LeetHub

--- 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
-        int mx=nums[0];
-        deque<int> dq;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            dq.empty()? nums[i]+=0: nums[i]+=dq.front();
-            mx=max(mx,nums[i]);
-            while(!dq.empty() && nums[i]>dq.back()) dq.pop_back();
-            if(nums[i]>0)   dq.push_back(nums[i]);
-            if(i>=k && !dq.empty() && nums[i-k]==dq.front())    dq.pop_front();
+        int res=nums[0], n=nums.size();
+        priority_queue<pair<int,int>> pq;
+        pq.push({res,0});
+        for(int i=1;i<n;i++){
+            while(i-pq.top().second>k)  pq.pop();
+            int mx=max(nums[i],pq.top().first+nums[i]);
+            res=max(mx,res);
+            pq.push({mx,i});
+
         }
-        return mx;
+        return res;
     }
 };
